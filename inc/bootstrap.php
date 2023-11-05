947c441d2ae63215c3132b85c3bee0927926d83e
Freddy Fabian <99642629+Freddy444@users.noreply.github.com>
2023-11-05 00:11:27 -0400

    Create bootstrap.php
    This PHP code serves as an entry point for a web application, defining constants, including configuration files, and loading essential components:

---
+++ inc/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+define("PROJECT_ROOT_PATH", "/Applications/XAMPP/xamppfiles/htdocs");
+
+require_once "./inc/config.php";
+require_once "./controller/Api/BaseController.php";
+require_once "/Applications/XAMPP/xamppfiles/htdocs/models/SongModel.php";
+require_once "./models/UserModel.php";
+?>
